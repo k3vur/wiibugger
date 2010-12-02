1e704c6f695581db38bdc303253abc737b845eed
Basil Kohler <basil.kohler@gmail.com>
2010-12-02 18:57:47 +0100

    partytime
    Signed-off-by: Basil Kohler <basil.kohler@gmail.com>

---
+++ src/WiiBugger.java
@@ -0,0 +1,12 @@
+
+public class WiiBugger {
+	public static void main(String[] args) {
+		printParty();
+	}
+	public static void printParty() {
+		System.out.println(putYourHandsUp());
+	}
+	public static String putYourHandsUp() {
+		return "\\m/ _m_ \\m/ _m_ \\m/";
+	}
+}
