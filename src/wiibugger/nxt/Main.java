4301c0cd40d617a10d350d9f984921091808e661
Kevin Urban <theonlyulti@gmail.com>
2011-01-07 15:26:15 +0100

    NXT: waits for connection

--- src/wiibugger/nxt/Main.java
+++ src/wiibugger/nxt/Main.java
@@ -1,12 +1,39 @@
 package wiibugger.nxt;
 
-import lejos.nxt.Button;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import lejos.nxt.Motor;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
 
 public class Main {
 
+	static BTConnection btConnection;
+	static DataInputStream inStream;
+	// DataOutputStream outStream;
+	
 	public static void main(String[] args) {
-		System.out.println("Wiibugger :D");
-		Button.waitForPress();
+		System.out.println("Welcome to Wiibugger");
+		
+		
+		while (true) {
+			System.out.println("Waiting for connection...");
+			btConnection = Bluetooth.waitForConnection();
+			inStream = btConnection.openDataInputStream();
+			
+			while(true) {
+				int angle;
+				try {
+					angle = (int)inStream.readShort();
+					Motor.A.rotateTo(angle);
+				} catch (IOException e) {
+					System.out.println("IOException");
+				}
+			}
+			
+		}
 	}
 
 }
