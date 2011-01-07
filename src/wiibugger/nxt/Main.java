905952e19b421dacc39bd43cb6a734b04800ca50
Kevin Urban <theonlyulti@gmail.com>
2011-01-07 02:02:04 +0100

    Added LeJOS libraries and ant-build scripts

---
+++ src/wiibugger/nxt/Main.java
@@ -0,0 +1,12 @@
+package wiibugger.nxt;
+
+import lejos.nxt.Button;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Wiibugger :D");
+		Button.waitForPress();
+	}
+
+}
---
+++ src/wiibugger/pc/Main.java
@@ -0,0 +1,9 @@
+package wiibugger.pc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Wiibugger PC");
+	}
+
+}
