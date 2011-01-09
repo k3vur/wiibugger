3823e9dbd6a0b532474a973b2fa345cde1cbe0ce
Kevin Urban <theonlyulti@gmail.com>
2011-01-09 04:02:09 +0100

    first user interface draft

---
+++ src/wiibugger/pc/Wiibugger.java
@@ -0,0 +1,72 @@
+package wiibugger.pc;
+
+import java.io.DataOutputStream;
+
+import lejos.pc.comm.NXTConnector;
+import wiibugger.pc.ui.UserInterface;
+
+public class Wiibugger {
+
+	static NXTConnector connector;
+	static DataOutputStream nxtOut;
+	
+	public static void main(String[] args) {
+		
+		/**
+		 * Some things can only be run as 32 bit mode (like Blue cove).
+		 * Exit if run as 64 bit mode.
+		 */
+		if (Wiibugger.runsAs64Bit()) {
+			System.out.println(
+					"Wiibugger can only run in 32 bit mode\n" +
+					"because it needs some 32 bit Libraries such as Bluecove.\n" +
+					"\n" +
+					"Make sure to run it in 32 bit mode by calling it with\n" + 
+					"\"java -d32 Wiibugger\"");
+			System.exit(0);
+		}
+		
+    	/*
+    	 * Wiimote uses pcm (something like Bluetooth "ports") < 4069
+    	 * which is not recommended and checked by bluecove.
+    	 * We have to turn that check off.
+    	 */
+    	System.setProperty("bluecove.jsr82.psm_minimum_off","true");
+    	
+    	/*
+    	 * Wiimote needs Widcomm Bluetooth stack under windows to communicate
+    	 */
+    	System.setProperty("bluecove.stack.first", "widcomm");
+    	
+    	UserInterface.init();
+    	
+		
+		/*
+		 * Wii Connection
+		 */
+//		System.out.println("Trying to connect wo WiiMote");
+//        //Find and connect to a Wii Remote
+//        WiiRemote remote = null;
+//        while (remote == null) {
+//            try {
+//                remote = WiiRemoteJ.findRemote();
+//            } catch(Exception e) {
+//                remote = null;
+//                e.printStackTrace();
+//                System.out.println("Failed to connect remote. Trying again.");
+//            }
+//        }
+
+	}
+
+	/**
+	 * Returns true if the program is being run in 64 bit mode,
+	 * and false if it isn't
+	 * 
+	 * @return true, if run in 64 bit mode, false if not.
+	 */
+	public static boolean runsAs64Bit() {
+		return System.getProperty("sun.arch.data.model").indexOf("64") != -1;
+	}
+
+}
---
+++ src/wiibugger/pc/ui/UserInterface.java
@@ -0,0 +1,157 @@
+package wiibugger.pc.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+
+public class UserInterface {
+	
+	private static Font labelFont;
+	private static JFrame mainWindow;
+	private static JButton runButton, scanWiimoteButton, scanNXTButton;
+	private static JSplitPane splitPane;
+	private static JLabel wiimoteLabel, nxtLabel;
+	private static JList wiimoteList, nxtList;
+	private static JPanel wiimotePanel, nxtPanel;
+	
+	private static Font getLabelFont() {
+		if (UserInterface.labelFont == null) {
+			labelFont = new Font(null, Font.BOLD, 18);
+		}
+		return UserInterface.labelFont;
+	}
+
+	private static JFrame getMainWindow() {
+		if (UserInterface.mainWindow == null) {
+			mainWindow = new JFrame("Wiibugger");
+			mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			mainWindow.setSize(800, 600);
+			
+			JPanel mainWindowPanel = new JPanel(new BorderLayout());
+			mainWindowPanel.add(getSplitPane(), BorderLayout.CENTER);
+			mainWindowPanel.add(getRunButton(), BorderLayout.SOUTH);
+			
+			mainWindow.add(mainWindowPanel);
+		}
+		return UserInterface.mainWindow;
+	}
+	
+	private static JLabel getNXTLabel() {
+		if (UserInterface.nxtLabel == null) {
+			nxtLabel = new JLabel("NXTs");
+			nxtLabel.setFont(getLabelFont());
+		}
+		return UserInterface.nxtLabel;
+	}
+
+	private static JList getNXTList() {
+		if (UserInterface.nxtList == null) {
+			nxtList = new JList();
+		}
+		return UserInterface.nxtList;
+	}
+	
+	private static JPanel getNXTPanel() {
+		if (UserInterface.nxtPanel == null) {
+			nxtPanel = new JPanel(new BorderLayout());
+			
+			JPanel topPanel = new JPanel(new BorderLayout());
+			topPanel.add(getNXTLabel(), BorderLayout.WEST);
+			topPanel.add(getScanNXTButton(), BorderLayout.EAST);
+			
+			nxtPanel.add(topPanel, BorderLayout.NORTH);			
+			nxtPanel.add(new JScrollPane(getNXTList()), BorderLayout.CENTER);
+		}
+		return UserInterface.nxtPanel;
+	}
+	
+	private static Component getRunButton() {
+		if (UserInterface.runButton == null) {
+			runButton = new JButton("Run Wiibugger!");
+		}
+		return UserInterface.runButton;
+	}
+
+	private static JButton getScanNXTButton() {
+		if (UserInterface.scanNXTButton == null) {
+			scanNXTButton = new JButton("Scan");
+			
+			// TODO nxt scanbutton actionlistener
+			scanNXTButton.addActionListener(null);
+		}
+		return UserInterface.scanNXTButton;
+	}
+
+	private static JButton getScanWiimotesButton() {
+		if (UserInterface.scanWiimoteButton == null) {
+			scanWiimoteButton = new JButton("Scan");
+			
+			// TODO wiimote scanbutton actionlistener
+			scanWiimoteButton.addActionListener(null);
+		}
+		return UserInterface.scanWiimoteButton;
+	}
+
+	private static JSplitPane getSplitPane() {
+		if (UserInterface.splitPane == null) {
+			splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+			splitPane.setLeftComponent(getWiimotePanel());
+			splitPane.setRightComponent(getNXTPanel());
+			splitPane.setDividerLocation(0.5);
+		}
+		return UserInterface.splitPane;
+	}
+
+	private static JLabel getWiiLabel() {
+		if (UserInterface.wiimoteLabel == null) {
+			wiimoteLabel = new JLabel("Wiimotes");
+			wiimoteLabel.setFont(getLabelFont());
+		}
+		return UserInterface.wiimoteLabel;
+	}
+	
+	private static JList getWiimoteList() {
+		if (UserInterface.wiimoteList == null) {
+			wiimoteList = new JList();
+		}
+		return UserInterface.wiimoteList;
+	}
+
+	private static JPanel getWiimotePanel() {
+		if (UserInterface.wiimotePanel == null) {
+			wiimotePanel = new JPanel(new BorderLayout());
+			
+			JPanel topPanel = new JPanel(new BorderLayout());
+			topPanel.add(getWiiLabel(), BorderLayout.WEST);
+			topPanel.add(getScanWiimotesButton(), BorderLayout.EAST);
+			
+			wiimotePanel.add(topPanel, BorderLayout.NORTH);			
+			wiimotePanel.add(new JScrollPane(getWiimoteList()), BorderLayout.CENTER);
+		}
+		return UserInterface.wiimotePanel;
+	}
+	
+	public static void init() {
+		
+    	/*
+    	 * Set native look and feel.
+    	 */
+    	try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("Could not use native look and feel");
+		} 
+		
+		getMainWindow().setVisible(true);
+	}
+	
+}
