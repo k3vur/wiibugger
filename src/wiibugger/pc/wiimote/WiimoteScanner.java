4297f51185441be6d54bd8fd6233c39a2735c5e0
Kevin Urban <theonlyulti@gmail.com>
2011-01-09 06:21:30 +0100

    Turn on all LEDs on connected Wiimote

--- src/wiibugger/pc/wiimote/WiimoteScanner.java
+++ src/wiibugger/pc/wiimote/WiimoteScanner.java
@@ -1,5 +1,7 @@
 package wiibugger.pc.wiimote;
 
+import java.io.IOException;
+
 import wiibugger.pc.DeviceList;
 import wiiremotej.WiiRemote;
 import wiiremotej.WiiRemoteJ;
@@ -29,22 +31,33 @@ public class WiimoteScanner extends Thread {
 		this.callAfterFinish = callAfterFinish;
 	}
 	
+	/**
+	 * Scan for wiimotes
+	 */
 	@Override
 	public void run() {
 		System.out.println("Scanning for Wiimotes...");
 		
 		WiiRemote remote = null;
 		for (int i = 0; i < numberOfScans; i++) {
+			
 			try {
 				remote = WiiRemoteJ.findRemote();
 			} catch (Exception e) {
 				// TODO: handle findRemote() exception
 				System.out.println("Exception while finding Wiimotes");
 			}
+			
 			if (remote != null) {
+				try {
+					remote.setLEDLights(new boolean[] { true, true, true, true });
+				} catch (Exception e) {
+					System.out.println("Could not set LED for " + remote.getBluetoothAddress());
+				} 
 				wiimoteList.add(remote);
 				remote = null;
 			}
+			
 		}
 		
 		if (callAfterFinish != null) {
